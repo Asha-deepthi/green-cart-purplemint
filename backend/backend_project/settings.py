12bb69317b98a843b438170d2c527f25911fe944
Asha Deepthi <202040257asha@gmail.com>
2025-08-12 23:16:44 +0530

    allowed hosts

--- backend/backend_project/settings.py
+++ backend/backend_project/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = env("SECRET_KEY", default="unsafe-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
+
 
 # Application definition
 INSTALLED_APPS = [
